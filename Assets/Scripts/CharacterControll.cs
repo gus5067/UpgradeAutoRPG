edea21a31db099167a12d7bf2f7375f7f2fb7f5d
gus5067 <tpgus120238@gmail.com>
2022-09-20 17:22:38 +0900

    fix : 캐릭터 navMeshAgent로 인해 위치 저장 안되던 문제 수정

--- Assets/Scripts/CharacterControll.cs
+++ Assets/Scripts/CharacterControll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterControll : MonoBehaviour
 {
@@ -9,12 +10,18 @@ public class CharacterControll : MonoBehaviour
 
     private void Awake()
     {
+        player.GetComponent<NavMeshAgent>().enabled = false;
+    }
+
+    private void Start()
+    {
         if (GameManager.Instance.characterPos != Vector3.zero)
         {
             player.transform.position = GameManager.Instance.characterPos;
 
             player.transform.rotation = Quaternion.Euler(GameManager.Instance.characterRotation);
         }
+        player.GetComponent<NavMeshAgent>().enabled = true;
     }
 
 }
